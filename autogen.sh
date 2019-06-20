9f4dfd237fd1737d6940aa7cc2e7b3bd66299d59
aalm <artturi.alm@gmail.com>
2019-06-20 10:18:29 +0300

    -portable version of OpenBSD cu(1) by nicm@ automagix/compat goo shifted from -portable of tmux(1) :] TODO decide what to do with remote(5)-support now #if0'ed

---
+++ autogen.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ "x$(uname)" = "xOpenBSD" ]; then
+	[ -z "$AUTOMAKE_VERSION" ] && export AUTOMAKE_VERSION=1.15
+	[ -z "$AUTOCONF_VERSION" ] && export AUTOCONF_VERSION=2.69
+fi
+
+die()
+{
+    echo "$@" >&2
+    exit 1
+}
+
+aclocal || die "aclocal failed"
+automake --add-missing --force-missing --copy --foreign || die "automake failed"
+autoreconf || die "autoreconf failed"
---
+++ compat.h
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2007 Nicholas Marriott <nicholas.marriott@gmail.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF MIND, USE, DATA OR PROFITS, WHETHER
+ * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef COMPAT_H
+#define COMPAT_H
+
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <sys/uio.h>
+
+#include <limits.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <termios.h>
+#include <wchar.h>
+
+#ifndef __GNUC__
+#define __attribute__(a)
+#endif
+
+#ifndef __unused
+#define __unused __attribute__ ((__unused__))
+#endif
+#ifndef __dead
+#define __dead __attribute__ ((__noreturn__))
+#endif
+#ifndef __packed
+#define __packed __attribute__ ((__packed__))
+#endif
+
+#ifdef HAVE_ERR_H
+#include <err.h>
+#else
+void	err(int, const char *, ...);
+void	errx(int, const char *, ...);
+void	warn(const char *, ...);
+void	warnx(const char *, ...);
+void	verr(int, const char *, va_list);
+void	verrx(int, const char *, va_list);
+#endif
+
+#ifndef HAVE_PATHS_H
+#define	_PATH_BSHELL	"/bin/sh"
+#define	_PATH_TMP	"/tmp/"
+#define _PATH_DEVNULL	"/dev/null"
+#define _PATH_TTY	"/dev/tty"
+#define _PATH_DEV	"/dev/"
+#endif
+
+#ifndef __OpenBSD__
+#define pledge(s, p) (0)
+#endif
+
+#ifdef HAVE_STDINT_H
+#include <stdint.h>
+#else
+#include <inttypes.h>
+#endif
+
+#ifdef HAVE_PATHS_H
+#include <paths.h>
+#endif
+
+#ifdef HAVE_VIS
+#include <vis.h>
+#else
+#include "compat/vis.h"
+#endif
+
+#ifndef HAVE_CLOSEFROM
+/* closefrom.c */
+void		 closefrom(int);
+#endif
+
+#ifndef HAVE_STRTONUM
+/* strtonum.c */
+long long	 strtonum(const char *, long long, long long, const char **);
+#endif
+
+#ifndef HAVE_GETPROGNAME
+/* getprogname.c */
+const char	*getprogname(void);
+#endif
+
+#ifndef HAVE_GETOPT
+/* getopt.c */
+extern int	BSDopterr;
+extern int	BSDoptind;
+extern int	BSDoptopt;
+extern int	BSDoptreset;
+extern char    *BSDoptarg;
+int	BSDgetopt(int, char *const *, const char *);
+#define getopt(ac, av, o)  BSDgetopt(ac, av, o)
+#define opterr             BSDopterr
+#define optind             BSDoptind
+#define optopt             BSDoptopt
+#define optreset           BSDoptreset
+#define optarg             BSDoptarg
+#endif
+
+#endif /* COMPAT_H */
